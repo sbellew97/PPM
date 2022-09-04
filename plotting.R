4cbb2a61c73479d6fa98ca15d1c5b2326c23151d
sbellew97 <63533215+sbellew97@users.noreply.github.com>
2022-09-04 14:32:18 +1000

    Add files via upload

---
+++ plotting.R
@@ -0,0 +1,275 @@
+totfitIPP1<-totfitIPP
+totfitIPPBT1<-totfitIPPBT
+totwlrfit1<-totwlrfit
+totlrbadd1<-totlrbadd
+totppmIPP1<-totppmIPP
+tmfitIPP1<-tmfitIPP
+tmfitIPPBT1<-tmfitIPPBT
+tmwlrfit1<-tmwlrfit
+tmlrbadd1<-tmlrbadd
+tmppmIPP1<-tmppmIPP
+rmsefitIPP1<-rmsefitIPP
+rmsefitIPPBT1<-rmsefitIPPBT
+rmsewlrfit1<-rmsewlrfit
+rmselrbadd1<-rmselrbadd
+rmseppmIPP1<-rmseppmIPP
+maxllfitIPP1<-maxllfitIPP
+maxllfitIPPBT1<-maxllfitIPPBT
+maxllwlrfit1<-maxllwlrfit
+maxlllrbadd1<-maxlllrbadd
+maxllppmIPP1<-maxllppmIPP
+summaryfitIPP1 <- summary.IPP
+summaryfitIPPBT1 <- summary.IPPBT
+summarywlrfit1 <- summary.wlr
+summarylrbadd1 <- summary.lr
+summaryppmIPP1 <- summary.ppm
+
+totfitIPP3<-totfitIPP
+totfitIPPBT3<-totfitIPPBT
+totwlrfit3<-totwlrfit
+totlrbadd3<-totlrbadd
+totppmIPP3<-totppmIPP
+tmfitIPP3<-tmfitIPP
+tmfitIPPBT3<-tmfitIPPBT
+tmwlrfit3<-tmwlrfit
+tmlrbadd3<-tmlrbadd
+tmppmIPP3<-tmppmIPP
+rmsefitIPP3<-rmsefitIPP
+rmsefitIPPBT3<-rmsefitIPPBT
+rmsewlrfit3<-rmsewlrfit
+rmselrbadd3<-rmselrbadd
+rmseppmIPP3<-rmseppmIPP
+maxllfitIPP3<-maxllfitIPP
+maxllfitIPPBT3<-maxllfitIPPBT
+maxllwlrfit3<-maxllwlrfit
+maxlllrbadd3<-maxlllrbadd
+maxllppmIPP3<-maxllppmIPP
+summaryfitIPP3 <- summary.IPP
+summaryfitIPPBT3 <- summary.IPPBT
+summarywlrfit3 <- summary.wlr
+summarylrbadd3 <- summary.lr
+summaryppmIPP3 <- summary.ppm
+
+totfitIPP5<-totfitIPP
+totfitIPPBT5<-totfitIPPBT
+totwlrfit5<-totwlrfit
+totlrbadd5<-totlrbadd
+totppmIPP5<-totppmIPP
+tmfitIPP5<-tmfitIPP
+tmfitIPPBT5<-tmfitIPPBT
+tmwlrfit5<-tmwlrfit
+tmlrbadd5<-tmlrbadd
+tmppmIPP5<-tmppmIPP
+rmsefitIPP5<-rmsefitIPP
+rmsefitIPPBT5<-rmsefitIPPBT
+rmsewlrfit5<-rmsewlrfit
+rmselrbadd5<-rmselrbadd
+rmseppmIPP5<-rmseppmIPP
+maxllfitIPP5<-maxllfitIPP
+maxllfitIPPBT5<-maxllfitIPPBT
+maxllwlrfit5<-maxllwlrfit
+maxlllrbadd5<-maxlllrbadd
+maxllppmIPP5<-maxllppmIPP
+summaryfitIPP5 <- summary.IPP
+summaryfitIPPBT5 <- summary.IPPBT
+summarywlrfit5 <- summary.wlr
+summarylrbadd5 <- summary.lr
+summaryppmIPP5 <- summary.ppm
+
+totfitIPP9<-totfitIPP
+totfitIPPBT9<-totfitIPPBT
+totwlrfit9<-totwlrfit
+totlrbadd9<-totlrbadd
+totppmIPP9<-totppmIPP
+tmfitIPP9<-tmfitIPP
+tmfitIPPBT9<-tmfitIPPBT
+tmwlrfit9<-tmwlrfit
+tmlrbadd9<-tmlrbadd
+tmppmIPP9<-tmppmIPP
+rmsefitIPP9<-rmsefitIPP
+rmsefitIPPBT9<-rmsefitIPPBT
+rmsewlrfit9<-rmsewlrfit
+rmselrbadd9<-rmselrbadd
+rmseppmIPP9<-rmseppmIPP
+maxllfitIPP9<-maxllfitIPP
+maxllfitIPPBT9<-maxllfitIPPBT
+maxllwlrfit9<-maxllwlrfit
+maxlllrbadd9<-maxlllrbadd
+maxllppmIPP9<-maxllppmIPP
+summaryfitIPP9 <- summary.IPP
+summaryfitIPPBT9 <- summary.IPPBT
+summarywlrfit9 <- summary.wlr
+summarylrbadd9 <- summary.lr
+summaryppmIPP9 <- summary.ppm
+
+totfitIPP10<-totfitIPP
+totfitIPPBT10<-totfitIPPBT
+totwlrfit10<-totwlrfit
+totlrbadd10<-totlrbadd
+totppmIPP10<-totppmIPP
+tmfitIPP10<-tmfitIPP
+tmfitIPPBT10<-tmfitIPPBT
+tmwlrfit10<-tmwlrfit
+tmlrbadd10<-tmlrbadd
+tmppmIPP10<-tmppmIPP
+rmsefitIPP10<-rmsefitIPP
+rmsefitIPPBT10<-rmsefitIPPBT
+rmsewlrfit10<-rmsewlrfit
+rmselrbadd10<-rmselrbadd
+rmseppmIPP10<-rmseppmIPP
+maxllfitIPP10<-maxllfitIPP
+maxllfitIPPBT10<-maxllfitIPPBT
+maxllwlrfit10<-maxllwlrfit
+maxlllrbadd10<-maxlllrbadd
+maxllppmIPP10<-maxllppmIPP
+summaryfitIPP10 <- summary.IPP
+summaryfitIPPBT10 <- summary.IPPBT
+summarywlrfit10 <- summary.wlr
+summarylrbadd10 <- summary.lr
+summaryppmIPP10 <- summary.ppm
+aucppmIPP15 <- aucppmIPP
+aucwlrfit15 <- aucwlrfit
+auclrbadd15 <- auclrbadd
+
+totfitIPP12<-totfitIPP
+totfitIPPBT12<-totfitIPPBT
+totwlrfit12<-totwlrfit
+totlrbadd12<-totlrbadd
+totppmIPP12<-totppmIPP
+tmfitIPP12<-tmfitIPP
+tmfitIPPBT12<-tmfitIPPBT
+tmwlrfit12<-tmwlrfit
+tmlrbadd12<-tmlrbadd
+tmppmIPP12<-tmppmIPP
+rmsefitIPP12<-rmsefitIPP
+rmsefitIPPBT12<-rmsefitIPPBT
+rmsewlrfit12<-rmsewlrfit
+rmselrbadd12<-rmselrbadd
+rmseppmIPP12<-rmseppmIPP
+maxllfitIPP12<-maxllfitIPP
+maxllfitIPPBT12<-maxllfitIPPBT
+maxllwlrfit12<-maxllwlrfit
+maxlllrbadd12<-maxlllrbadd
+maxllppmIPP12<-maxllppmIPP
+summaryfitIPP12 <- summary.IPP
+summaryfitIPPBT12 <- summary.IPPBT
+summarywlrfit12 <- summary.wlr
+summarylrbadd12 <- summary.lr
+summaryppmIPP12 <- summary.ppm
+
+totfitIPP15<-totfitIPP
+totfitIPPBT15<-totfitIPPBT
+totwlrfit15<-totwlrfit
+totlrbadd15<-totlrbadd
+totppmIPP15<-totppmIPP
+tmfitIPP15<-tmfitIPP
+tmfitIPPBT15<-tmfitIPPBT
+tmwlrfit15<-tmwlrfit
+tmlrbadd15<-tmlrbadd
+tmppmIPP15<-tmppmIPP
+rmsefitIPP15<-rmsefitIPP
+rmsefitIPPBT15<-rmsefitIPPBT
+rmsewlrfit15<-rmsewlrfit
+rmselrbadd15<-rmselrbadd
+rmseppmIPP15<-rmseppmIPP
+maxllfitIPP15<-maxllfitIPP
+maxllfitIPPBT15<-maxllfitIPPBT
+maxllwlrfit15<-maxllwlrfit
+maxlllrbadd15<-maxlllrbadd
+maxllppmIPP15<-maxllppmIPP
+summaryfitIPP15 <- summary.IPP
+summaryfitIPPBT15 <- summary.IPPBT
+summarywlrfit15 <- summary.wlr
+summarylrbadd15 <- summary.lr
+summaryppmIPP15 <- summary.ppm
+
+totfitIPP20<-totfitIPP
+totfitIPPBT20<-totfitIPPBT
+totwlrfit20<-totwlrfit
+totlrbadd20<-totlrbadd
+totppmIPP20<-totppmIPP
+tmfitIPP20<-tmfitIPP
+tmfitIPPBT20<-tmfitIPPBT
+tmwlrfit20<-tmwlrfit
+tmlrbadd20<-tmlrbadd
+tmppmIPP20<-tmppmIPP
+rmsefitIPP20<-rmsefitIPP
+rmsefitIPPBT20<-rmsefitIPPBT
+rmsewlrfit20<-rmsewlrfit
+rmselrbadd20<-rmselrbadd
+rmseppmIPP20<-rmseppmIPP
+maxllfitIPP20<-maxllfitIPP
+maxllfitIPPBT20<-maxllfitIPPBT
+maxllwlrfit20<-maxllwlrfit
+maxlllrbadd20<-maxlllrbadd
+maxllppmIPP20<-maxllppmIPP
+summaryfitIPP20 <- summary.IPP
+summaryfitIPPBT20 <- summary.IPPBT
+summarywlrfit20 <- summary.wlr
+summarylrbadd20 <- summary.lr
+summaryppmIPP20 <- summary.ppm
+
+totfitIPP30<-totfitIPP
+totfitIPPBT30<-totfitIPPBT
+totwlrfit30<-totwlrfit
+totlrbadd30<-totlrbadd
+totppmIPP30<-totppmIPP
+tmfitIPP30<-tmfitIPP
+tmfitIPPBT30<-tmfitIPPBT
+tmwlrfit30<-tmwlrfit
+tmlrbadd30<-tmlrbadd
+tmppmIPP30<-tmppmIPP
+rmsefitIPP30<-rmsefitIPP
+rmsefitIPPBT30<-rmsefitIPPBT
+rmsewlrfit30<-rmsewlrfit
+rmselrbadd30<-rmselrbadd
+rmseppmIPP30<-rmseppmIPP
+maxllfitIPP30<-maxllfitIPP
+maxllfitIPPBT30<-maxllfitIPPBT
+maxllwlrfit30<-maxllwlrfit
+maxlllrbadd30<-maxlllrbadd
+maxllppmIPP30<-maxllppmIPP
+summaryfitIPP30 <- summary.IPP
+summaryfitIPPBT30 <- summary.IPPBT
+summarywlrfit30 <- summary.wlr
+summarylrbadd30 <- summary.lr
+summaryppmIPP30 <- summary.ppm
+
+totfitIPP40<-totfitIPP
+totfitIPPBT40<-totfitIPPBT
+totwlrfit40<-totwlrfit
+totlrbadd40<-totlrbadd
+totppmIPP40<-totppmIPP
+tmfitIPP40<-tmfitIPP
+tmfitIPPBT40<-tmfitIPPBT
+tmwlrfit40<-tmwlrfit
+tmlrbadd40<-tmlrbadd
+tmppmIPP40<-tmppmIPP
+rmsefitIPP40<-rmsefitIPP
+rmsefitIPPBT40<-rmsefitIPPBT
+rmsewlrfit40<-rmsewlrfit
+rmselrbadd40<-rmselrbadd
+rmseppmIPP40<-rmseppmIPP
+maxllfitIPP40<-maxllfitIPP
+maxllfitIPPBT40<-maxllfitIPPBT
+maxllwlrfit40<-maxllwlrfit
+maxlllrbadd40<-maxlllrbadd
+maxllppmIPP40<-maxllppmIPP
+summaryfitIPP40 <- summary.IPP
+summaryfitIPPBT40 <- summary.IPPBT
+summarywlrfit40 <- summary.wlr
+summarylrbadd40 <- summary.lr
+summaryppmIPP40 <- summary.ppm
+
+plot(resolution,c(rmsefitIPP1,rmsefitIPP2,rmsefitIPP5,rmsefitIPP10,rmsefitIPP15),type='o',col='black',xlab='n0',ylab='rmse',main='rmse v n0')
+lines(resolution,c(rmselrbadd1,rmselrbadd2,rmselrbadd5,rmselrbadd10,rmselrbadd15),type='o',col='red')
+lines(resolution,c(rmsewlrfit1,rmsewlrfit2,rmsewlrfit5,rmsewlrfit10,rmsewlrfit15),type='o',col='green')
+lines(resolution,c(rmseppmIPP1,rmseppmIPP2,rmseppmIPP5,rmseppmIPP10,rmseppmIPP15),type='o',col='blue')
+legend("topright",c("IPP","weighted log","Baddeley","ppm"), fill=c("black","green","red","blue") ,bty="n")
+
+plot(resolution,c(rmsefitIPP1,rmsefitIPP2,rmsefitIPP5,rmsefitIPP10,rmsefitIPP15),type='o',col='black',xlab='n0',ylab='rmse',main='rmse v n0')
+lines(resolution,c(rmselrbadd1,rmselrbadd2,rmselrbadd5,rmselrbadd10,rmselrbadd15),type='o',col='red')
+lines(resolution,c(rmsewlrfit1,rmsewlrfit2,rmsewlrfit5,rmsewlrfit10,rmsewlrfit15),type='o',col='green')
+lines(resolution,c(rmseppmIPP1,rmseppmIPP2,rmseppmIPP5,rmseppmIPP10,rmseppmIPP15),type='o',col='blue')
+legend("topright",c("IPP","weighted log","Baddeley","ppm"), fill=c("black","green","red","blue") ,bty="n")
+
